14666b15fe84348709c3a94eaf8081927de3d13e
Артур <arthur.betsa@gmail.com>
2020-05-05 18:53:38 +0300

    to github

--- main.js
+++ main.js
@@ -2,56 +2,35 @@
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const pop_up = document.getElementById("pop-up");
 
-    document.getElementById("prediction").addEventListener("click", event => {
-        // let answers = Math.ceil(Math.random() * 8);
-        //         // let message = "";
-        //         //
-        //         // if (answers === 1) message = "It is certain";
-        //         // if (answers === 2) message = "Outlook good";
-        //         // if (answers === 3) message = "You may rely on it";
-        //         // if (answers === 4) message = "Ask again later";
-        //         // if (answers === 5) message = "Concentrate and ask again";
-        //         // if (answers === 6) message = "Reply hazy, try again";
-        //         // if (answers === 7) message = "My reply is no";
-        //         // if (answers === 8) message = "My sources say no";
-        //         //
-        //         // let pop_up_message =
-        //         //     `<div class="message">
-        //         //     <p>${message}</p>
-        //         //     <div class="pop_close"></div>
-        //         // </div>`;
-        //         //
-        //         // pop_up.innerHTML += pop_up_message;
+    document.getElementById("prediction").addEventListener("click", createPrediction);
 
+    function createPrediction() {
+        const id = `f${(~~(Math.random() * 1e8)).toString(16)}`;
 
-    });
+        let prediction = new renderPrediction(id);
+        prediction.rendrer();
+    }
 
     document.addEventListener("click", toClose);
-    
+
     function toClose(event) {
         let close = event.target.className;
         let parent = event.target.offsetParent;
-        if(close == "pop_close"){
-            // parent.style = "display: none";
+        if (close == "pop_close") {
             parent.remove();
+            createPrediction.some;
         }
     }
-
-
-
-
-
-
 
 
     class renderPrediction {
 
         constructor(id) {
+            this.pop_up = document.getElementById("pop-up");
+            this.pop_up_message = "";
             this.id = id;
         }
-
 
         returnPrediction() {
             let answers = Math.ceil(Math.random() * 8);
@@ -65,81 +44,34 @@ document.addEventListener("DOMContentLoaded", () => {
             if (answers === 6) message = "Reply hazy, try again";
             if (answers === 7) message = "My reply is no";
             if (answers === 8) message = "My sources say no";
-
             return message;
         }
 
-        let pop_up_message =
-            `<div class="message">
-            <p>${returnPrediction()}</p>
-            <div class="pop_close"></div>
-        </div>`;
+        rendrer() {
+            this.pop_up_message = `<div class="message" id="${this.id}">
+                <p>${this.returnPrediction()}</p>
+                <div class="pop_close"></div>
+            </div>`;
+            this.pop_up.innerHTML += this.pop_up_message;
+            setTimeout(() => {
+                this.delete_message()
+            }, 5000);
+        }
 
-        pop_up.innerHTML += pop_up_message;
-
+        delete_message() {
+            const close = document.getElementById(`${this.id}`);
+            close != undefined?close.remove(): false;
+        }
 
 
     }
-
-
-
-
 
 
 });
 
 
 
-class renderPrediction {
-
-    constructor(id) {
-        this.id = id;
-    }
-
-
-    returnPrediction() {
-        let answers = Math.ceil(Math.random() * 8);
-        let message = "";
-
-        if (answers === 1) message = "It is certain";
-        if (answers === 2) message = "Outlook good";
-        if (answers === 3) message = "You may rely on it";
-        if (answers === 4) message = "Ask again later";
-        if (answers === 5) message = "Concentrate and ask again";
-        if (answers === 6) message = "Reply hazy, try again";
-        if (answers === 7) message = "My reply is no";
-        if (answers === 8) message = "My sources say no";
-
-        return message;
-    }
-
-    let pop_up_message =
-        `<div class="message">
-            <p>${returnPrediction()}</p>
-            <div class="pop_close"></div>
-        </div>`;
-
-    pop_up.innerHTML += pop_up_message;
 
 
 
-}
-
-
-
-function returnPrediction() {
-    let answers = Math.ceil(Math.random() * 8);
-    let message = "";
-
-    if (answers === 1) message = "It is certain";
-    if (answers === 2) message = "Outlook good";
-    if (answers === 3) message = "You may rely on it";
-    if (answers === 4) message = "Ask again later";
-    if (answers === 5) message = "Concentrate and ask again";
-    if (answers === 6) message = "Reply hazy, try again";
-    if (answers === 7) message = "My reply is no";
-    if (answers === 8) message = "My sources say no";
-    
-    return message;
-}
 
