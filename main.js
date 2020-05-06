54872e2e6cee3ff467315517eb815a2fc9b8f3ac
Артур <arthur.betsa@gmail.com>
2020-05-06 23:07:29 +0300

    fixes

--- main.js
+++ main.js
@@ -2,94 +2,63 @@
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    function makeCounter() {
-        var currentCount = 0;
+    let prediction = {};
+    const pop_up = document.getElementById("pop-up");
+    const returnID = () => `f${(~~(Math.random() * 1e8)).toString(16)}`;
+    const answers = () => Math.ceil(Math.random() * 8 - 1);
+    let predictionMessage = ["It is certain", "Outlook good",
+        "You may rely on it", "Ask again later",
+        "Concentrate and ask again", "Reply hazy, try again",
+        "My reply is no", "My sources say no"];
 
-        // возвращаемся к функции
-        function counter() {
-            return currentCount;
+    let currentCount = {  //current count of active messages
+        value: 0,
+        minus: function () {
+            this.value--
+        },
+        plus: function () {
+            this.value++
         }
-
-
-        counter.minus = function () {
-            currentCount--;
-        };
-        counter.plus = function () {
-            currentCount++;
-        };
-
-
-
-        return counter;
     };
-
-    let count = makeCounter();
 
 
     document.getElementById("prediction").addEventListener("click", createPrediction);
 
     function createPrediction() {  //create prediction by button
-        const id = `f${(~~(Math.random() * 1e8)).toString(16)}`;
 
-        let prediction = new renderPrediction(id);
+        prediction = new renderPrediction();
         prediction.rendrer();
-
-        count.plus();
-        if(count() === 6){
-            let test = document.getElementById("pop-up").firstChild.remove();
-            count.minus();
-        }
-
-    }
-
-    document.addEventListener("click", toClose);  //close massage my click
-
-    function toClose(event) {
-        let close = event.target.className;
-        let parent = event.target.offsetParent;
-        if (close == "pop_close") {
-            parent.remove();
-            count.minus();
+        if (currentCount.value === 6) {
+            pop_up.firstChild.remove();
+            currentCount.minus();
         }
     }
+
+    document.addEventListener("click", event => { //close massage by click
+        event.target.className == "pop_close"
+            ? prediction.delete_message(event.target.offsetParent)
+            : false
+    });
 
 
     class renderPrediction {
-
-        constructor(id) {
-            this.pop_up = document.getElementById("pop-up");
-            this.pop_up_message = "";
-            this.id = id;
+        constructor() {
+            this.id = returnID();
             this.timer;
         }
 
-        returnPrediction() { //create prediction
-            let answers = Math.ceil(Math.random() * 8);
-            let message = "";
 
-            if (answers === 1) message = "It is certain";
-            if (answers === 2) message = "Outlook good";
-            if (answers === 3) message = "You may rely on it";
-            if (answers === 4) message = "Ask again later";
-            if (answers === 5) message = "Concentrate and ask again";
-            if (answers === 6) message = "Reply hazy, try again";
-            if (answers === 7) message = "My reply is no";
-            if (answers === 8) message = "My sources say no";
-            return message;
-        }
-
-        rendrer() { //render new predoction message
-            this.pop_up_message = `<div class="message" id="${this.id}">
-                <p>${this.returnPrediction()}</p>
+        rendrer() { //render new prediction message
+            currentCount.plus();
+            let pop_up_message = `<div class="message" id="${this.id}">
+                <p>${predictionMessage[answers()]}</p>
                 <div class="pop_close" ></div>
             </div>`;
-            this.pop_up.innerHTML += this.pop_up_message;
+            pop_up.innerHTML += pop_up_message;
             setTimeout(() => {
                 this.delete_message()
             }, 5000);
             this.toFade();
-
-
         }
 
         toFade() {  //fade color
@@ -100,27 +69,18 @@ document.addEventListener("DOMContentLoaded", () => {
                         .style.backgroundColor = `rgba(131, 255, 64, ${color})`;
                     color -= 0.02;
                 }
-
             }, 100)
-
         }
 
-
-        delete_message() { //delete message
-            const close = document.getElementById(`${this.id}`);
-            if (close != undefined) {
+        delete_message(parent) { //delete message
+            const close = parent || document.getElementById(`${this.id}`);
+            if (close) {
                 close.remove();
-                count.minus();
+                currentCount.minus();
             }
-
-
             clearTimeout(this.timer);
         }
-
-
     }
-
-
 });
 
 
